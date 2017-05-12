f2a0f93d647c0588fb16bfa6885fbca2d082ed75
Krish <Krish@Chaitanya.home>
2017-05-12 22:02:21 +0530

    commit by krish

---
+++ HelloWorld/src/com/abc/test/Test.java
@@ -0,0 +1,14 @@
+package com.abc.test;
+
+import com.abc.model.Employee;
+
+public class Test {
+	public static void main(String[] args) {
+		Employee employee=new Employee();
+		employee.setId(1);
+		employee.setName("mahap");
+		employee.setEmail("mail");
+		System.out.println("Id is "+employee.getId()+"Name is "+employee.getName()+"email is"+employee.getEmail());
+	}
+
+}
